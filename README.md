31e2055e3d544526f70d0c98195285d8c9e16f8c
Triplesong90 <expert190@outlook.kr>
2023-10-17 13:58:55 +0900

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+docker run -it --gpus all -m=10g -v /mnt/volume1/strap-segmentation:/mnt/volume1/strap-segmentation yolov8-seg
--- src/train.py
+++ src/train.py
@@ -31,7 +31,7 @@ def train():
         for i in range(len(LABELS)):
             f.write(f"  {i}: {LABELS[i]}\n")
 
-    model.train(data="./train.yaml", epochs=800, workers=1, batch=20, imgsz=640, patience=0,
+    model.train(data="./train.yaml", epochs=800, workers=1, batch=40, imgsz=640, patience=0,
                 hsv_h = 0,
                 hsv_s = 0,
                 hsv_v = 0.05,
