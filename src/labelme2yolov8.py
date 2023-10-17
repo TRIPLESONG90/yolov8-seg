70b1277dc270f3056d18500e86bef3a74187c63f
Triplesong90 <expert190@outlook.kr>
2023-10-17 13:29:10 +0900

    update

--- src/labelme2yolov8.py
+++ src/labelme2yolov8.py
@@ -3,49 +3,47 @@ import os
 import json
 import shutil
 import cv2
+from tqdm import tqdm
 
-SRC_DIR = "dataset"
-DST_DIR = "dataset_yolo"
-LABELS = ["OK"]
-
-os.path.isdir(DST_DIR) or os.makedirs(DST_DIR)
+def convert_label(SRC_DIR, DST_DIR, LABELS):
+    os.path.isdir(DST_DIR) or os.makedirs(DST_DIR)
 
 
-jsons = glob.glob(f'{SRC_DIR}/*.json')
-imgs = glob.glob(f'{SRC_DIR}/*.png')
+    jsons = glob.glob(f'{SRC_DIR}/*.json')
+    imgs = glob.glob(f'{SRC_DIR}/*.png')
 
-dst_img_dir = os.path.join(DST_DIR, "images")
-os.path.isdir(dst_img_dir) or os.makedirs(dst_img_dir)
-dst_label_dir = os.path.join(DST_DIR, "labels")
-os.path.isdir(dst_label_dir) or os.makedirs(dst_label_dir)
+    dst_img_dir = os.path.join(DST_DIR, "images")
+    os.path.isdir(dst_img_dir) or os.makedirs(dst_img_dir)
+    dst_label_dir = os.path.join(DST_DIR, "labels")
+    os.path.isdir(dst_label_dir) or os.makedirs(dst_label_dir)
 
 
-dst_imgs = []
-for i in range(len(jsons)):
-    json_file = jsons[i]
-    img_file = imgs[i]
-    j = json.load(open(json_file, 'r'))
+    dst_imgs = []
+    for i in tqdm(range(len(jsons)), desc="Converting labelme format to yolo format"):
+        json_file = jsons[i]
+        img_file = imgs[i]
+        j = json.load(open(json_file, 'r'))
 
-    dst_txt_path = os.path.join(dst_label_dir, os.path.splitext(os.path.basename(json_file))[0] + ".txt")
-    dst_img_path = os.path.join(dst_img_dir, os.path.basename(img_file))
+        dst_txt_path = os.path.join(dst_label_dir, os.path.splitext(os.path.basename(json_file))[0] + ".txt")
+        dst_img_path = os.path.join(dst_img_dir, os.path.basename(img_file))
 
-    os.path.isfile(dst_img_path) or shutil.copy(img_file, dst_img_path)
+        os.path.isfile(dst_img_path) or shutil.copy(img_file, dst_img_path)
 
-    img = cv2.imread(img_file)
-    height, width, _ = img.shape
+        img = cv2.imread(img_file)
+        height, width, _ = img.shape
 
-    dst_imgs.append(dst_img_path)
-    with open(dst_txt_path, 'w') as f:
-        for shape in j["shapes"]:
-            index = LABELS.index(shape["label"])
+        dst_imgs.append(dst_img_path)
+        with open(dst_txt_path, 'w') as f:
+            for shape in j["shapes"]:
+                index = LABELS.index(shape["label"])
 
-            pts = ""
-            for point in shape["points"]:
-                pts += f'{point[0] / width} {point[1] / height} '
+                pts = ""
+                for point in shape["points"]:
+                    pts += f'{point[0] / width} {point[1] / height} '
 
-            line = f'{index} {pts}\n'
-            f.write(line.strip())
+                line = f'{index} {pts}\n'
+                f.write(line.strip())
 
-with open(os.path.join(DST_DIR, "test.txt"), 'w') as f:
-    for img in dst_imgs:
-        f.write(img + "\n")
+    with open(os.path.join(DST_DIR, "test.txt"), 'w') as f:
+        for img in dst_imgs:
+            f.write(img + "\n")
--- src/train.py
+++ src/train.py
@@ -1,9 +1,35 @@
 from multiprocessing import freeze_support
 from ultralytics import YOLO
 
+from labelme2yolov8 import convert_label
+import os
+import time
 def train():
+    SRC_DIR = "/mnt/volume1/strap-segmentation"
+    DST_DIR = "dataset"
+    LABELS = ["OK"]
+
+    os.path.isdir(DST_DIR) and os.system(f"rm -rf {DST_DIR}")
+
+    convert_label(SRC_DIR, DST_DIR, LABELS)
 
     model = YOLO("./yolov8n-seg.pt", "segmentation")
+
+    #get full path of DST_DIR
+    DST_DIR = os.path.abspath(DST_DIR)
+
+    #remove old train.yaml
+    os.path.isfile("train.yaml") and os.system("rm train.yaml")
+
+    #save train.yaml
+    with open("train.yaml", 'w') as f:
+        f.write(f"path: {DST_DIR}\n")
+        f.write(f"train: test.txt\n")
+        f.write(f"val: test.txt\n")
+        f.write(f"test: test.txt\n")
+        f.write(f"names: \n")
+        for i in range(len(LABELS)):
+            f.write(f"  {i}: {LABELS[i]}\n")
 
     model.train(data="./train.yaml", epochs=800, workers=1, batch=20, imgsz=640, patience=0,
                 hsv_h = 0,
